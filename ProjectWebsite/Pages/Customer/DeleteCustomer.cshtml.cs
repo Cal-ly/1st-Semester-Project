661a5697f1c6206a461827b42b68618228a7f5a9
Cal-ly <cal-ly@outlook.dk>
2023-11-27 10:52:23 +0100

    Updated all CustomerPages

--- ProjectWebsite/Pages/Customer/DeleteCustomer.cshtml.cs
+++ ProjectWebsite/Pages/Customer/DeleteCustomer.cshtml.cs
@@ -2,30 +2,31 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using ProjectWebsite.Services;
 
+/*
+TO BE IMPLEMENTED: 
+PROPERTIES: Change name of service to match implemented service
+CONSTRUCTOR: Change name of service to match implemented service
+METHOD: Change name of service to match implemented service
+
+Check if the customer is null, if so, return NotFound page
+Check accces modifiers!
+*/
+
 namespace ProjectWebsite.Pages.Customer
 {
     public class DeleteCustomerModel : PageModel
     {
-		/*
-		TO BE IMPLEMENTED: 
-		PROPERTIES: Change name of service to match implemented service
-		CONSTRUCTOR: Change name of service to match implemented service
-		METHOD: Change name of service to match implemented service
-
-		Check if the customer is null, if so, return NotFound page
-		Check accces modifiers!
-		*/
 
 		public JsonFileCustomerService CustomerService;
 
-		[BindProperty]
-		public Models.Customer Customer { get; set; }
-		public List<Models.Customer> Customers { get; set; } //Used for displaying all customers
 
-		public CreateCustomerModel(CustomerService service)
+		public DeleteCustomerModel(CustomerService service)
 		{
 			CustomerService = service;
 		}
+		[BindProperty]
+		public Models.Customer Customer { get; set; }
+		public List<Models.Customer> Customers { get; set; } //Used for displaying all customers
 
 		public IActionResult OnGet()
 		{
@@ -38,7 +39,7 @@ namespace ProjectWebsite.Pages.Customer
 			if (deletedCustomer == null)
 				return RedirectToPage("/Error"); //Define NotFound page
 
-			return RedirectToPage("GetAllItems");
+			return RedirectToPage("GetAllCustomers");
 		}
 	}
 }
