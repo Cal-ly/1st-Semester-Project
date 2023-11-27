4c622eb0086516ca34a27b797bf0015fbca3987a
Cal-ly <cal-ly@outlook.dk>
2023-11-27 10:24:43 +0100

    Updated CreateCustomerPage

--- ProjectWebsite/Pages/Customer/CreateCustomer.cshtml.cs
+++ ProjectWebsite/Pages/Customer/CreateCustomer.cshtml.cs
@@ -5,8 +5,30 @@ namespace ProjectWebsite.Pages.Customer
 {
     public class CreateCustomerModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		//to be implemented
+		private JsonFileCustomerService CustomerService;
+
+		[BindProperty]
+		public Models.Customer Customer { get; set; }
+
+		public CreateCustomerModel(CustomerService service)
+		{
+			CustomerService = service;
+		}
+
+		public IActionResult OnGet()
+		{
+			return Page();
+		}
+
+		public IActionResult OnPost()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
+			CustomerService.AddObject(Customer);
+			return RedirectToPage("./Index");
+		}
+	}
 }
