a42dabee6d1a605fd918fe5537bea18b00fe28ee
MathiasNoergaard <man010@edu.zealand.dk>
2023-11-27 11:40:18 +0100

    implemted OrderLog Changed access modifier in Order class

---
+++ ProjectWebsite/Models/OrderLog.cs
@@ -0,0 +1,24 @@
+﻿namespace ProjectWebsite.Models
+{
+    public class OrderLog
+    {
+        private List<Order> orderLog { get; set; }
+        
+        public Order SearchOrder(int orderID)
+        {
+            foreach (Order order in orderLog)
+                if (order.ID == orderID)
+                    return order;
+            return null;
+        }
+
+        public List<Order> GetCustomerOrders(int customerID)
+        {
+            List<Order> temp = new();
+            foreach (Order order in orderLog)
+                if (order.CustomerID == customerID)
+                    temp.Add(order);
+            return temp;
+        }
+    }
+}
