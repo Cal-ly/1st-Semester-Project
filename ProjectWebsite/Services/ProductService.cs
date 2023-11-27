92dbee0834dc14f0002a719eb14926c933c2d19b
Monicatoep <69761812+Monicatoep@users.noreply.github.com>
2023-11-27 12:45:00 +0100

    Created ProductService

---
+++ ProjectWebsite/Services/ProductService.cs
@@ -0,0 +1,30 @@
+﻿using ProjectWebsite.Models;
+using ProjectWebsite.Services;
+namespace ProjectWebsite.Services
+
+{
+    public class ProductService
+    {
+        private static List<Product> Products = new List<Product>();
+        private JsonProductService JsonProductService { get; set; }
+        public ProductService(JsonProductService jsonProductService)
+        {
+            JsonProductService = jsonProductService;
+            Products = JsonProductService.GetJsonItems().ToList();
+        }
+
+        public IEnumerable<Product> PriceFilter(int maxPrice, int minPrice = 0)
+        {
+            List<Product> filterList = new List<Product>();
+            foreach (Product product in Products)
+            {
+                if ((minPrice == 0 && product.Price <= maxPrice) || (maxPrice == 0 && product.Price >= minPrice) || (product.Price >= minPrice && product.Price <= maxPrice))
+                {
+                    filterList.Add(product);
+                }
+            }
+
+            return filterList;
+        }
+    }
+}
