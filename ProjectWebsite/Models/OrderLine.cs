bc37d9c7908a39ccbc00a52fa977ee48f16c888a
lausalber <150662642+lausalber@users.noreply.github.com>
2023-11-28 10:17:22 +0100

    Tilføjet til order og orderline

---
+++ ProjectWebsite/Models/OrderLine.cs
@@ -0,0 +1,27 @@
+﻿namespace ProjectWebsite.Models
+{
+    public class OrderLine
+    {
+        public enum PackageSize
+        {
+            _100 = 100, //assign a value to each enum
+            _150 = 150,
+            _200 = 200,
+            _300 = 300
+        }
+
+        public Product Product { get; set; }
+        public PackageSize SelectedPackage { get; set; }
+        public int Amount { get; set; }
+
+        public OrderLine()
+        {
+            
+        }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Product)}={Product}, {nameof(Amount)}={Amount.ToString()}}}";
+        }
+    }
+}
