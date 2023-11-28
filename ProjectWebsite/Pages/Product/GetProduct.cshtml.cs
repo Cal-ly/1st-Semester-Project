a43f537e145c5264df890045816fb71c6e5c817c
Monicatoep <69761812+Monicatoep@users.noreply.github.com>
2023-11-28 10:18:58 +0100

    Made changes to GetProduct

--- ProjectWebsite/Pages/Product/GetProduct.cshtml.cs
+++ ProjectWebsite/Pages/Product/GetProduct.cshtml.cs
@@ -1,12 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectWebsite.Services;
 
 namespace ProjectWebsite.Pages.Product
 {
     public class GetProductModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        public Models.Product Product { get; set; }
+        private ProductRepository _productRepository;
+
+        public GetProductModel(ProductRepository productRepository)
         {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Product = _productRepository.GetProduct(id);
+            if (Product == null)
+                return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
+
+            return Page();
         }
     }
 }
