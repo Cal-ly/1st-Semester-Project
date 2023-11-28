a7bf1f48a93bb62bfc0fb922f94bc363ee04679b
MathiasNoergaard <man010@edu.zealand.dk>
2023-11-28 12:49:39 +0100

    Trying to implement add to basket functionality

--- ProjectWebsite/Models/OrderLine.cs
+++ ProjectWebsite/Models/OrderLine.cs
@@ -2,16 +2,8 @@
 {
     public class OrderLine
     {
-        public enum PackageSize
-        {
-            _100 = 100, //assign a value to each enum
-            _150 = 150,
-            _200 = 200,
-            _300 = 300
-        }
-
         public Product Product { get; set; }
-        public PackageSize SelectedPackage { get; set; }
+       // public PackageSize SelectedPackage { get; set; }
         public int Amount { get; set; }
 
         public OrderLine()
--- ProjectWebsite/Pages/Product/GetProduct.cshtml.cs
+++ ProjectWebsite/Pages/Product/GetProduct.cshtml.cs
@@ -10,6 +10,18 @@ namespace ProjectWebsite.Pages.Product
         public Models.Product Product { get; set; }
         private ProductRepository _productRepository;
 
+        [BindProperty]
+        public string bool0 { get; set; }
+        [BindProperty]
+        public string bool1 { get; set; }
+        [BindProperty]
+        public string bool2 { get; set; }
+        [BindProperty]
+        public string bool3 { get; set; }
+        [BindProperty]
+        public int amountIN { get; set; }
+
+
         public GetProductModel(ProductRepository productRepository)
         {
             _productRepository = productRepository;
@@ -21,6 +33,18 @@ namespace ProjectWebsite.Pages.Product
             if (Product == null)
                 return RedirectToPage("/NotFound"); //NotFound er ikke defineret endnu
 
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            Console.WriteLine("Testing23");
+            Console.WriteLine(bool0);
+            Console.WriteLine(bool1);
+            Console.WriteLine(bool2);
+            Console.WriteLine(bool3);
+            Console.WriteLine(amountIN);
+            
             return Page();
         }
     }
