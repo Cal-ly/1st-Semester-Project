926c08f98968680ff60caa7841fd8605274bc2a4
Cal-ly <cal-ly@outlook.dk>
2023-12-01 13:23:35 +0100

    Changed datatypes in Event

--- ProjectWebsite/Models/Event.cs
+++ ProjectWebsite/Models/Event.cs
@@ -6,9 +6,11 @@
         public string EventName { get; set; }
         public string EventDescription { get; set; }
         public string EventLocation { get; set; }
-        public string EventDate { get; set; }
-        public string EventTime { get; set; }
-        public string EventDuration { get; set; }
+        public DateTime EventDate { get; set; }
+        public DateTime EventTime { get; set; }
+        //public double EventCost { get; set; }
+        //public List<Customer> EventAttendees { get; set; }
+        public int EventDuration { get; set; } // in mintues
         public string EventOrganizer { get; set; }
         public string EventOrganizerEmail { get; set; }
         public string EventOrganizerPhone { get; set; }
@@ -16,35 +18,9 @@
         
         public Event() {}
 
-        public override string ToString()
-        {
-            return $"{{{nameof(ID)}={ID.ToString()}, {nameof(EventName)}={EventName}, {nameof(EventDescription)}={EventDescription}, {nameof(EventLocation)}={EventLocation}, {nameof(EventDate)}={EventDate}, {nameof(EventTime)}={EventTime}, {nameof(EventDuration)}={EventDuration}, {nameof(EventOrganizer)}={EventOrganizer}, {nameof(EventOrganizerEmail)}={EventOrganizerEmail}, {nameof(EventOrganizerPhone)}={EventOrganizerPhone}, {nameof(EventOrganizerWebsite)}={EventOrganizerWebsite}}}";
-        }
-
-        //public Event(
-        //    int id,
-        //    string eventName, 
-        //    string eventDescription, 
-        //    string eventLocation, 
-        //    string eventDate, 
-        //    string eventTime, 
-        //    string eventDuration, 
-        //    string eventOrganizer, 
-        //    string eventOrganizerEmail, 
-        //    string eventOrganizerPhone, 
-        //    string eventOrganizerWebsite)
-        //{
-        //    ID = id;
-        //    EventName = eventName;
-        //    EventDescription = eventDescription;
-        //    EventLocation = eventLocation;
-        //    EventDate = eventDate;
-        //    EventTime = eventTime;
-        //    EventDuration = eventDuration;
-        //    EventOrganizer = eventOrganizer;
-        //    EventOrganizerEmail = eventOrganizerEmail;
-        //    EventOrganizerPhone = eventOrganizerPhone;
-        //    EventOrganizerWebsite = eventOrganizerWebsite;
-        //}
-    }
+		//public override string ToString()
+		//{
+		//	return $"{{{nameof(ID)}={ID.ToString()}, {nameof(EventName)}={EventName}, {nameof(EventDescription)}={EventDescription}, {nameof(EventLocation)}={EventLocation}, {nameof(EventDate)}={EventDate.ToString()}, {nameof(EventTime)}={EventTime.ToString()}, {nameof(EventCost)}={EventCost.ToString()}, {nameof(EventAttendees)}={EventAttendees}, {nameof(EventDuration)}={EventDuration.ToString()}, {nameof(EventOrganizer)}={EventOrganizer}, {nameof(EventOrganizerEmail)}={EventOrganizerEmail}, {nameof(EventOrganizerPhone)}={EventOrganizerPhone}, {nameof(EventOrganizerWebsite)}={EventOrganizerWebsite}}}";
+		//}
+	}
 }
