4407403dc13694ff3a476c70cc9e75dc82669519
Cal-ly <cal-ly@outlook.dk>
2023-12-01 12:23:28 +0100

    Update to Events

--- ProjectWebsite/Pages/Events/GetAllEvents.cshtml.cs
+++ ProjectWebsite/Pages/Events/GetAllEvents.cshtml.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using ProjectWebsite.Models;
 using ProjectWebsite.Services;
 
 namespace ProjectWebsite.Pages.Events
@@ -8,17 +7,16 @@ namespace ProjectWebsite.Pages.Events
     public class GetAllEventsModel : PageModel
     {
         public EventService service;
-        [BindProperty] public Event Event { get; set; }
         [BindProperty] public string SearchEvent { get; set; }
-        [BindProperty] public List<Event> EventList { get; set; }
+        [BindProperty] public List<Models.Event> EventList { get; set; }
 
         public GetAllEventsModel(EventService eventService)
         {
             service = eventService;
         }
-        public IActionResult OnGet() { return Page(); }
+        public void OnGet() { EventList = service.EventList; }
 
-        public IActionResult OnPost()
+        public IActionResult OnPostEventSearch()
         {
             if (!ModelState.IsValid) { return Page(); }
             EventList = service.GetEventsByName(SearchEvent).ToList();
