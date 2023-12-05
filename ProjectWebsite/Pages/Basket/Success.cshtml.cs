334754129c0badfe35f4e6381981b6d74b41852b
MathiasNoergaard <man010@edu.zealand.dk>
2023-12-05 12:11:05 +0100

    Made placeorder to a bool

---
+++ ProjectWebsite/Pages/Basket/Success.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectWebsite.Pages.Basket
+{
+    public class SuccessModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
