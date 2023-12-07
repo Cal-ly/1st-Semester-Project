401a839e48c825dd761a8eeec08d182da5f16683
Cal-ly <cal-ly@outlook.dk>
2023-12-07 10:43:30 +0100

    Added comments to all "Customer" related code

--- ProjectWebsite/Pages/Customer/UpdateCustomer.cshtml.cs
+++ ProjectWebsite/Pages/Customer/UpdateCustomer.cshtml.cs
@@ -13,18 +13,26 @@ namespace ProjectWebsite.Pages.Customer
 		{
 			CustomerService = customerService;
 		}
+		//Denne metode bliver kaldt når der trykkes på "Update" knappen på "GetAllCustomers" siden.
 		public IActionResult OnGet(int id)
 		{
-			Customer = CustomerService.GetCustomerByID(id);
+            //Henter kunden fra CustomerService
+            Customer = CustomerService.GetCustomerByID(id);
+            //Der bliver tjekket om kunden er null, hvis den er null bliver der redirected til NotFound siden.
 			if (Customer == null) { return RedirectToPage("/NotFound"); }
             return Page();
 		}
+		//Denne metode bliver kaldt når der trykkes på "Update" knappen på "UpdateCustomer" siden.
 		public IActionResult OnPost()
 		{
+			//Hvis modellen ikke er valid, bliver siden vist igen.
 			if (!ModelState.IsValid) { return Page(); }
+			//Opdaterer kunden i CustomerService
             CustomerService.UpdateCustomer(Customer);
+			//Redirecter tilbage til "GetAllCustomers" siden.
 			return RedirectToPage("GetAllCustomers");
 		}
+		//Redirecter tilbage til "GetAllCustomers" siden, hvis der trykkes på "Cancel".
         public IActionResult OnPostCancel() { return RedirectToPage("GetAllCustomers"); }
 	}
 }
