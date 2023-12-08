bca07cbb41b2788f201ecf965e09bb4dafbe293b
MathiasNoergaard <man010@edu.zealand.dk>
2023-12-08 09:26:59 +0100

    Checkpoint

--- ProjectWebsite/Pages/Index.cshtml.cs
+++ ProjectWebsite/Pages/Index.cshtml.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectWebsite.Services;
 
 namespace ProjectWebsite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public ProductService ProductService { get; set; }
+        public EventService EventService { get; set; }
+
+        public IndexModel(ProductService productService, EventService eventService)
+        {
+            ProductService = productService;
+            EventService = eventService;
+        }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
