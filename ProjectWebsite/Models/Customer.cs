b66a99025a48d4cfe085042c50f5312903d9cc6d
MathiasNoergaard <man010@edu.zealand.dk>
2023-12-08 11:36:16 +0100

    Added placeholder text in the Models

--- ProjectWebsite/Models/Customer.cs
+++ ProjectWebsite/Models/Customer.cs
@@ -4,18 +4,19 @@ namespace ProjectWebsite.Models
 {
     public class Customer
     {
-        [Display(Name = "Kunde ID")]
+        [Display(Name = "Kunde ID", Prompt="Indtast et kunde ID")]
         public int ID { get; set; }
 
-        [Display(Name = "Navn")]
+        [Display(Name = "Navn", Prompt = "Indtast et navn")]
         public string Name { get; set; }
 
-        [Display(Name = "Adresse")]
+        [Display(Name = "Adresse", Prompt = "Indtast en adresse")]
         public string Address { get; set; }
 
-		public string Email { get; set; }
+        [Display(Name = "E-mail", Prompt = "Indtast en e-mail adresse")]
+        public string Email { get; set; }
 
-        [Display(Name = "Telefonnummer")]
+        [Display(Name = "Telefonnummer", Prompt = "Indtast et telefonnummer")]
         public string PhoneNumber { get; set; }
 
         public Customer()
--- ProjectWebsite/Models/Event.cs
+++ ProjectWebsite/Models/Event.cs
@@ -4,45 +4,39 @@ namespace ProjectWebsite.Models
 {
     public class Event
     {
-        [Display(Name = "Event ID")]
+        [Display(Name = "Event ID", Prompt = "Indtast et event ID")]
         public int ID { get; set; }
 
-        [Display(Name = "Navn")]
+        [Display(Name = "Navn", Prompt = "Indtast et navn for eventet")]
         public string Name { get; set; }
 
-        [Display(Name = "Sted")]
+        [Display(Name = "Sted", Prompt = "Indtast et sted")]
         public string Location { get; set; }
 
-        [Display(Name = "Dato")]
+        [Display(Name = "Dato", Prompt = "Indtast en dato")]
         public DateTime DateTime { get; set; }
 
-        [Display(Name = "Varighed (minutter)")]
-
+        [Display(Name = "Varighed (minutter)", Prompt = "Indtast eventets længde i minutter")]
         [Range(1, int.MaxValue)]
         public int Duration { get; set; } // in mintues
 
-        [Display(Name = "Beskrivelse")]
+        [Display(Name = "Beskrivelse", Prompt = "Indtast en beskrivelse")]
         public string Description { get; set; }
 
-        [Display(Name = "Pris")]
-        //[Display(Name = "Event pris")]
-        //[Required(ErrorMessage = "Der skal angives en pris")]
-        //[Range(typeof(double), "1", "10000", ErrorMessage = "Pris skal være over 0")]
+        [Display(Name = "Pris", Prompt = "Indtast en pris (mindst 0 kr.)")]
         [Range(0, double.MaxValue)]
         public double Cost { get; set; }
 
         [Display(Name = "Deltagere")]
         public List<Customer> Attendees { get; set; } = new();
 
-        [Display(Name = "Kapacitet")]
-        public List<Customer> Attendees { get; set; } = new();
-
+        [Display(Name = "Pris", Prompt = "Indtast en pris (mindst 0 kr.)")]
         [Range(1, int.MaxValue)]
         public int Capacity { get; set; }
 
         public bool IsFull => Attendees == null || Attendees.Count >= Capacity; // Checks if the event is full based on the number of attendees and the event capacity
 
-        [Display(Name = "Arrangør")]
+        [Display(Name = "Arrangør", Prompt = "Indtast navnet på arrangøren")]
         public string Organizer { get; set; }
 
         public Event()
