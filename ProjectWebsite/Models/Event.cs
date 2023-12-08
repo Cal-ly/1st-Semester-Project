10f1f337b429be2d85fdf2087de46128df077786
lausalber <150662642+lausalber@users.noreply.github.com>
2023-12-08 11:21:53 +0100

    Display name event

--- ProjectWebsite/Models/Event.cs
+++ ProjectWebsite/Models/Event.cs
@@ -4,16 +4,36 @@ namespace ProjectWebsite.Models
 {
     public class Event
     {
+        [Display(Name = "Event ID")]
         public int ID { get; set; }
+
+        [Display(Name = "Navn")]
         public string Name { get; set; }
+
+        [Display(Name = "Sted")]
         public string Location { get; set; }
+
+        [Display(Name = "Dato")]
         public DateTime DateTime { get; set; }
+
+        [Display(Name = "Varighed (minutter)")]
         public int Duration { get; set; } // in mintues
+
+        [Display(Name = "Beskrivelse")]
         public string Description { get; set; }
+
+        [Display(Name = "Pris")]
         public double Cost { get; set; }
-		public List<Customer> Attendees { get; set; } = new();
+
+        [Display(Name = "Deltagere")]
+        public List<Customer> Attendees { get; set; } = new();
+
+        [Display(Name = "Kapacitet")]
         public int Capacity { get; set; }
+
         public bool IsFull => Attendees == null || Attendees.Count >= Capacity; // Checks if the event is full based on the number of attendees and the event capacity
+
+        [Display(Name = "Arrangør")]
         public string Organizer { get; set; }
 
         public Event()
