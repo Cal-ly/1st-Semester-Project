e7a6e218ec5889db1b423d1ea2b9b4a922e67657
MathiasNoergaard <man010@edu.zealand.dk>
2023-12-08 12:40:09 +0100

    Moved ID in orderline to the top

--- ProjectWebsite/Models/OrderLine.cs
+++ ProjectWebsite/Models/OrderLine.cs
@@ -4,9 +4,9 @@ namespace ProjectWebsite.Models
 {
     public class OrderLine
     {
+        public int ID { get; set; }
         public Product Product { get; set; }
         public int Amount { get; set; }
-        public int ID { get; set; }
 
         public OrderLine()
         {
