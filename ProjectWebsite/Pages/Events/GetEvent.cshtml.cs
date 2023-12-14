45e7a7f5f273c9451022a97a5e3ddfe1c73c55ab
Cal-ly <cal-ly@outlook.dk>
2023-12-14 10:23:38 +0100

    There is now a AddEventToBasket in the GetEvent page

--- ProjectWebsite/Pages/Events/GetEvent.cshtml.cs
+++ ProjectWebsite/Pages/Events/GetEvent.cshtml.cs
@@ -8,11 +8,16 @@ namespace ProjectWebsite.Pages.Events
     public class GetEventModel : PageModel
     {
         private EventService EventService { get; set; }
+        private ProductService ProductService { get; set; }
+        private OrderService OrderService { get; set; }
+        [BindProperty]
         public Event Event { get; set; }
 
-        public GetEventModel(EventService eventService)
+        public GetEventModel(EventService eventService, ProductService productService, OrderService orderService)
         {
             EventService = eventService;
+            ProductService = productService;
+            OrderService = orderService;
         }
 
         public IActionResult OnGet(int id)
@@ -22,6 +27,22 @@ namespace ProjectWebsite.Pages.Events
             {
                 return RedirectToPage("/NotFound");
             }
+            return Page();
+        }
+
+        public IActionResult OnPostAddEventToBasket(int postid)
+        {
+            int newID = (Order.Basket?.Count == null || Order.Basket?.Count == 0) ? 1 : Order.Basket!.Max(p => p.ID) + 1;
+
+            Order.Basket?.Add(new()
+            {
+                Product = ProductService.GetProduct(postid),
+                Amount = 1,
+                ID = newID
+            });
+
+            Event = EventService.GetEventByID(postid);
+
             return Page();
         }
     }
