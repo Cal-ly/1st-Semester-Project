8d6ecb07d1bc5f33e563b863c18cc379a6217ff9
lausalber <150662642+lausalber@users.noreply.github.com>
2023-12-15 12:46:24 +0100

    tilføjet tilføj til kurv på produkt listen

--- ProjectWebsite/Pages/Product/GetAllProducts.cshtml.cs
+++ ProjectWebsite/Pages/Product/GetAllProducts.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ProjectWebsite.Models;
 using ProjectWebsite.Services;
 using System.ComponentModel.DataAnnotations;
 
@@ -8,7 +9,9 @@ namespace ProjectWebsite.Pages.Product
     public class GetAllProductsModel : PageModel
     {
         private ProductService ProductService { get; set; }
+        public Models.Product Product { get; set; }
         public List<Models.Product> Products { get; private set; }
+        public string Message { get; set; }
 
         [BindProperty]
         [Range(typeof(int), minimum: "0", maximum: "1000", ErrorMessage = "Prisen skal være mellem {1} og {2}")]
@@ -32,6 +35,51 @@ namespace ProjectWebsite.Pages.Product
         {
             Products = ProductService.ProductList;
         }
+
+        public IActionResult OnPostAddToBasket(int ID)
+        {
+            Models.Product product = ProductService.GetProduct(ID);
+            if (!ModelState.IsValid)
+            {
+                Product = product;
+                return Page();
+            }
+            int newID;
+            if (Order.Basket?.Count == null || Order.Basket?.Count == 0)
+            {
+                newID = 1;
+            }
+            else
+            {
+                newID = Order.Basket.Max(p => p.ID) + 1;
+            }
+
+            OrderLine temp = new()
+            {
+                Amount = 1,
+                Product = product,
+                ID = newID
+            };
+
+            foreach (OrderLine line in Order.Basket)
+            {
+                if (line.Product == product)
+                {
+                    line.Amount += temp.Amount;
+                    Message = "Antal opdateret";
+                    Product = product;
+                    Products = ProductService.ProductList;
+                    return Page();
+                }
+            }
+            Order.Basket.Add(temp);
+            Message = "Produkt tilføjet til kurven";
+            Product = product;
+            Products = ProductService.ProductList;
+            return Page();
+        }
+    
+
 
         public IActionResult OnPostPriceFilter()
         {
